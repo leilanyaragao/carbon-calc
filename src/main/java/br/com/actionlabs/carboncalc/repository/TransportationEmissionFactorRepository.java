8d65709d4fce3e9b48df5563b9a3e440da08cb08
leilanyaragao <leilanyaragao26@gmail.com>
2024-11-17 22:13:05 -0300

    [implementation]: add findByType in TransportationEmissionFactorRepository

--- src/main/java/br/com/actionlabs/carboncalc/repository/TransportationEmissionFactorRepository.java
+++ src/main/java/br/com/actionlabs/carboncalc/repository/TransportationEmissionFactorRepository.java
@@ -1,10 +1,13 @@
 package br.com.actionlabs.carboncalc.repository;
 
-import br.com.actionlabs.carboncalc.enums.TransportationType;
 import br.com.actionlabs.carboncalc.model.TransportationEmissionFactor;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface TransportationEmissionFactorRepository
-    extends MongoRepository<TransportationEmissionFactor, TransportationType> {}
+        extends MongoRepository<TransportationEmissionFactor, String> {
+    Optional<TransportationEmissionFactor> findByType(String type);
+}
