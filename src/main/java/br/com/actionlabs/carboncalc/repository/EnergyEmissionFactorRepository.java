9c9091cf424719b7617c732a0ecfcd5b96203770
leilanyaragao <leilanyaragao26@gmail.com>
2024-11-17 22:12:15 -0300

    [implementation]: add findByUf in EnergyEmissionFactorRepository

--- src/main/java/br/com/actionlabs/carboncalc/repository/EnergyEmissionFactorRepository.java
+++ src/main/java/br/com/actionlabs/carboncalc/repository/EnergyEmissionFactorRepository.java
@@ -5,7 +5,9 @@ import br.com.actionlabs.carboncalc.model.EnergyEmissionFactor;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface EnergyEmissionFactorRepository extends MongoRepository<EnergyEmissionFactor, String> {
-
+    Optional<EnergyEmissionFactor> findByUf(String uf);
 }
