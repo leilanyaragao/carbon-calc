2f373db23df8187e15b7814fdd727b4304ececf7
leilanyaragao <leilanyaragao26@gmail.com>
2024-11-17 22:13:33 -0300

    [implementation]: add annotation AllArgsConstructor

--- src/main/java/br/com/actionlabs/carboncalc/dto/TransportationDTO.java
+++ src/main/java/br/com/actionlabs/carboncalc/dto/TransportationDTO.java
@@ -1,9 +1,11 @@
 package br.com.actionlabs.carboncalc.dto;
 
 import br.com.actionlabs.carboncalc.enums.TransportationType;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class TransportationDTO {
   private TransportationType type;
   private int monthlyDistance;
