d27edecf1295eaca0c3454cd8e25a20e1bbd6e77
leilanyaragao <leilanyaragao26@gmail.com>
2024-11-17 22:12:33 -0300

    [implementation]: controller implementations

--- src/main/java/br/com/actionlabs/carboncalc/rest/OpenRestController.java
+++ src/main/java/br/com/actionlabs/carboncalc/rest/OpenRestController.java
@@ -1,8 +1,11 @@
 package br.com.actionlabs.carboncalc.rest;
 
 import br.com.actionlabs.carboncalc.dto.*;
+import br.com.actionlabs.carboncalc.service.CarbonCalculatorService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,20 +15,30 @@ import org.springframework.web.bind.annotation.*;
 @Slf4j
 public class OpenRestController {
 
-  @PostMapping("start-calc")
+    @Autowired
+    private CarbonCalculatorService carbonCalculatorService;
+
+    @PostMapping("start-calc")
   public ResponseEntity<StartCalcResponseDTO> startCalculation(
       @RequestBody StartCalcRequestDTO request) {
-    throw new RuntimeException("Not implemented");
+        String id = carbonCalculatorService.startCalculation(request);
+        StartCalcResponseDTO responseDTO = new StartCalcResponseDTO();
+        responseDTO.setId(id);
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseDTO);
   }
 
   @PutMapping("info")
   public ResponseEntity<UpdateCalcInfoResponseDTO> updateInfo(
       @RequestBody UpdateCalcInfoRequestDTO request) {
-    throw new RuntimeException("Not implemented");
+      carbonCalculatorService.updateCalculation(request);
+      UpdateCalcInfoResponseDTO responseDTO = new UpdateCalcInfoResponseDTO();
+      responseDTO.setSuccess(true);
+      return ResponseEntity.ok(responseDTO);
   }
 
   @GetMapping("result/{id}")
   public ResponseEntity<CarbonCalculationResultDTO> getResult(@PathVariable String id) {
-    throw new RuntimeException("Not implemented");
+      CarbonCalculationResultDTO resultDTO = carbonCalculatorService.calculateCarbonFootprint(id);
+      return ResponseEntity.ok(resultDTO);
   }
 }
